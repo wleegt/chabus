88d06b86367230f876902a556fc9385cb934c3d8
wleegt <wleegt@gmail.com>
2017-03-30 23:12:32 +0900

    delete imgae files when removing user

--- modules/users/client/controllers/admin/user.client.controller.js
+++ modules/users/client/controllers/admin/user.client.controller.js
@@ -9,7 +9,6 @@ angular.module('users.admin').controller('UserController', ['$scope', '$state', 
       if (confirm('Are you sure you want to delete this user?')) {
         if (user) {
           user.$remove();
-
           $scope.users.splice($scope.users.indexOf(user), 1);
         } else {
           $scope.user.$remove(function () {
--- modules/users/server/controllers/admin.server.controller.js
+++ modules/users/server/controllers/admin.server.controller.js
@@ -4,6 +4,7 @@
  * Module dependencies.
  */
 var path = require('path'),
+  fs = require('fs'),
   mongoose = require('mongoose'),
   User = mongoose.model('User'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
@@ -53,6 +54,48 @@ exports.delete = function (req, res) {
 
     res.json(user);
   });
+
+  /// delete all image files when removing user
+  var imageUrlsToDelete = [];
+  if (user.profileImageURL !== User.schema.path('profileImageURL').defaultValue) {
+    imageUrlsToDelete.push(user.profileImageURL);
+  }
+  for (var i = 0; i < user.busImageURLs.length; i++) {
+    if (user.busImageURLs[i] !== './modules/users/client/img/custom/defaultBus.png') {
+      imageUrlsToDelete.push(user.busImageURLs[i]);
+    }
+  }
+  if (user.licenseImageURL !== User.schema.path('licenseImageURL').defaultValue) {
+    imageUrlsToDelete.push(user.licenseImageURL);
+  }
+  if (user.registrationImageURL !== User.schema.path('registrationImageURL').defaultValue) {
+    imageUrlsToDelete.push(user.registrationImageURL);
+  }
+  // console.log(imageUrlsToDelete);
+
+  if (imageUrlsToDelete.length > 0) {
+    function deleteFiles(files, callback){
+      var i = files.length;
+      files.forEach(function(filepath){
+        fs.unlink(filepath, function(err) {
+          i--;
+          if (err) {
+            callback(err);
+            return;
+          } else if (i <= 0) {
+            callback(null);
+          }
+        });
+      });
+    }
+    deleteFiles(imageUrlsToDelete, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('all files removed');
+      }
+    });
+  }
 };
 
 /**
